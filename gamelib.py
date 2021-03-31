af89a1c134bd9ae2a0bad053671c4ea05af3cac0
kikikrittamate <kikikrittamate@gmail.com>
2021-03-31 21:43:26 +0700

    flyweight

--- gamelib.py
+++ gamelib.py
@@ -40,36 +40,41 @@ class Text(GameCanvasElement):
 
     def init_canvas_object(self):
         self.canvas_object_id = self.canvas.create_text(
-            self.x, 
+            self.x,
             self.y,
             text=self.text)
 
     def set_text(self, text):
         self.text = text
         self.canvas.itemconfigure(self.canvas_object_id, text=text)
-        
+
 
 class Sprite(GameCanvasElement):
-    def __init__(self, game_app, image_filename, x=0, y=0):
+    def __init__(self, game_app, image_filename, x=0, y=0, photo_image=None):
         self.image_filename = image_filename
+
+        self.photo_image = photo_image
+
         super().__init__(game_app, x, y)
 
     def init_canvas_object(self):
-        self.photo_image = tk.PhotoImage(file=self.image_filename)
+        if not self.photo_image:
+            self.photo_image = tk.PhotoImage(file=self.image_filename)
+
         self.canvas_object_id = self.canvas.create_image(
-            self.x, 
+            self.x,
             self.y,
             image=self.photo_image)
 
 
-class GameApp(ttk.Frame): 
+class GameApp(ttk.Frame):
     def __init__(self, parent, canvas_width=800, canvas_height=500, update_delay=33):
         super().__init__(parent)
         self.parent = parent
-        
+
         self.canvas_width = canvas_width
         self.canvas_height = canvas_height
-        
+
         self.update_delay = update_delay
 
         self.grid(sticky="news")
@@ -80,10 +85,10 @@ class GameApp(ttk.Frame):
 
         self.parent.bind('<KeyPress>', self.on_key_pressed)
         self.parent.bind('<KeyRelease>', self.on_key_released)
-        
+
     def create_canvas(self):
         self.canvas = tk.Canvas(self, borderwidth=0,
-            width=self.canvas_width, height=self.canvas_height, 
+            width=self.canvas_width, height=self.canvas_height,
             highlightthickness=0)
         self.canvas.grid(sticky="news")
 
